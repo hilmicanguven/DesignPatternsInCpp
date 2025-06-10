88eb55a3576b8a9e940ec93bc452e269bf4de8fc
hilmi.guven <hilmi.guven@stm.com.tr>
2025-06-10 17:24:20 +0300

    Added folder structure and SOLID principles

---
+++ SOLID/solid_principles.hpp
@@ -0,0 +1,21 @@
+/* 
+
+SOLID Design Principles
+
+  - Single Responsibility Principle:
+        Your class or method should have only one reason to change.
+        Your class or method should have only one responsibility to take care of.
+  
+  - Open-Closed Principle:
+        Software entities (classes, modules, functions, and so on) should be open for extension, but closed for modification.
+  
+  - Liskov Substitution Principle:
+        Derived or child classes must be substitutable for their base or parent classes.
+  
+  - Interface Segregation Principle:
+        Do not force any client to implement an interface which is irrelevant to them.
+  
+  - Dependency Inversion Principle:
+        High-level modules should not depend on low-level modules. Both should depend on abstractions.
+
+*/
